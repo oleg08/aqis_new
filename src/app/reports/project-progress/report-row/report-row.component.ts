3ec5b7167cf9f83cb7e5277d5e472454632c2aa7
Oleg Gudak <olhudak@gmail.com>
2019-02-24 16:03:51 +0200

    master: Reports: Projects progress and status - rework loading report

--- src/app/reports/project-progress/report-row/report-row.component.ts
+++ src/app/reports/project-progress/report-row/report-row.component.ts
@@ -16,42 +16,53 @@ export class ReportRowComponent implements OnInit {
 
   project: ReportProgress;
   load_completed = false;
+  load_error: boolean;
   msgs: Message[] = [];
   @Input() project_id: number;
 
   constructor(private messageService: MessageService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.loadProject();
+  }
+
+  loadProject() {
     const self = this;
+    self.load_error = false;
     self.http.get(`${environment.serverUrl}/reports_project_progress/${self.project_id}.json`).subscribe(
       res => {
 
-        const report: ProjectReport = res['report'];
+        if (res['report']) {
+          const report: ProjectReport = res['report'];
 
-        const num_companies = report.companies.length;
-        self.project = {
-          project: report.name,
-          total_companies: num_companies
-        };
-        const companies_with_active_step: CompaniesReport[] = report.companies.filter( c => c.step_active === true);
-        let project_progress = 0;
-        companies_with_active_step.forEach(ac => {
-          const progress: number = ((ac.step_order - 1) / ac.step_count) * 100;
-          project_progress += progress;
-        });
-        project_progress /= num_companies;
-        project_progress = Number(project_progress.toFixed(2));
-        if (project_progress <= 25) self.project.progress0_25 = project_progress;
-        if (project_progress > 25 && project_progress <= 50) self.project.progress25_50 = project_progress;
-        if (project_progress > 50 && project_progress <= 75) self.project.progress50_75 = project_progress;
-        if (project_progress > 75 && project_progress <= 100) self.project.progress75_100 = project_progress;
+          const num_companies = report.companies.length;
+          self.project = {
+            project: report.name,
+            total_companies: num_companies
+          };
+          const companies_with_active_step: CompaniesReport[] = report.companies.filter( c => c.step_active === true);
+          let project_progress = 0;
+          companies_with_active_step.forEach(ac => {
+            const progress: number = ((ac.step_order - 1) / ac.step_count) * 100;
+            project_progress += progress;
+          });
+          project_progress /= num_companies;
+          project_progress = Number(project_progress.toFixed(2));
+          if (project_progress <= 25) self.project.progress0_25 = project_progress;
+          if (project_progress > 25 && project_progress <= 50) self.project.progress25_50 = project_progress;
+          if (project_progress > 50 && project_progress <= 75) self.project.progress50_75 = project_progress;
+          if (project_progress > 75 && project_progress <= 100) self.project.progress75_100 = project_progress;
 
-        self.project.active_companies = report.companies.filter(c => c.active_state === true || c.active_state === null).length;
-        self.project.closed_companies = report.companies
-          .filter(c => c.active_state === false && (c.success_state === false || c.success_state === null)).length;
-        self.project.success_companies = report.companies.filter(c => c.success_state === true).length;
-        self.load_completed = true;
-        },
+          self.project.active_companies = report.companies.filter(c => c.active_state === true || c.active_state === null).length;
+          self.project.closed_companies = report.companies
+            .filter(c => c.active_state === false && (c.success_state === false || c.success_state === null)).length;
+          self.project.success_companies = report.companies.filter(c => c.success_state === true).length;
+          self.load_completed = true;
+        } else {
+          self.load_error = true;
+        }
+
+      },
       err => {
         self.messageService.add({severity: 'warn', summary: 'Warning', detail: `Can't load data`});
         console.log('err - ', err);
