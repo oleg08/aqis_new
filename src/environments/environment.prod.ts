2db0ea063b0ddde12e806751e8fcf8faac5a4420
Oleg Gudak <olhudak@gmail.com>
2019-03-14 15:26:42 +0200

    master: switching to production not working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  my_var: true,
   clientUrl: 'https://aqis-new.herokuapp.com',
   serverUrl: 'https://aqis-new-server.herokuapp.com',
   token_auth_config: {
