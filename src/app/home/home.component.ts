a509cd071c60488afdbd946cb2764494219bc4d5
Oleh Hudak <olhudak@gmail.com>
2019-07-24 11:32:31 +0300

    create-super-admin-US199: Create user by Super Admin - sign up with google

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../services/auth.service';
 import { CurrentUserService } from '../services/current-user.service';
@@ -16,6 +17,7 @@ export class HomeComponent implements OnInit {
 
   constructor(private elementRef: ElementRef,
               public rd: Renderer2,
+              public http: HttpClient,
               public authService: AuthService,
               private currentUser: CurrentUserService) { }
 
@@ -28,4 +30,7 @@ export class HomeComponent implements OnInit {
     this.el.nativeElement.submit();
   }
 
+  logInWithGoogle() {
+    this.http.get(`${environment.serverUrl}/auth/0/callback`);
+  }
 }
