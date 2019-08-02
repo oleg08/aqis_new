17225f5f3474efbeedb9810df2bd2a4a5812eab2
Oleh Hudak <olhudak@gmail.com>
2019-08-02 08:34:13 +0300

    social-registration: Social Registration - error auth is not a function

--- src/app/services/google-auth.service.ts
+++ src/app/services/google-auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { auth } from 'firebase/app';
-// import { AngularFireAuth } from '@angular/fire/auth';
+// import { auth } from 'firebase/app';
+import { auth } from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from './auth.service';
 import { GetKeywordService } from '../google-signin/get-keyword.service';
 
@@ -15,29 +16,29 @@ export interface LoginData {
 export class GoogleAuthService {
 
   constructor(
-    // public afAuth: AngularFireAuth,
+    public afAuth: AngularFireAuth,
     private getKeywordSrv: GetKeywordService,
     public authService: AuthService
   ) { }
 
   GoogleAuth() {
-    // return this.AuthLogin(new auth.GoogleAuthProvider());
+    return this.AuthLogin(new auth.GoogleAuthProvider());
   }
 
   AuthLogin(provider) {
     const self = this;
-    // return this.afAuth.auth.signInWithPopup(provider)
-    //   .then((result) => {
-    //     if (result.additionalUserInfo) {
-    //       const email: string = result.additionalUserInfo.profile['email'];
-    //       let psw: string;
-    //       self.getKeywordSrv.get(email).then(keyword => psw = keyword).then(res => {
-    //         self.freeLogin({ login: email, password: psw });
-    //       });
-    //     }
-    //   }).catch((error) => {
-    //     console.log(error);
-    //   });
+    return this.afAuth.auth.signInWithPopup(provider)
+      .then((result) => {
+        if (result.additionalUserInfo) {
+          const email: string = result.additionalUserInfo.profile['email'];
+          let psw: string;
+          self.getKeywordSrv.get(email).then(keyword => psw = keyword).then(res => {
+            self.freeLogin({ login: email, password: psw });
+          });
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
   }
 
   freeLogin(data: LoginData) {
