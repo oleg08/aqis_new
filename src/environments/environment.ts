a759bd26eec07d5e93d59ed64faef9cb74d2d0d4
Oleh Hudak <olhudak@gmail.com>
2019-08-07 10:46:32 +0300

    master: Update npm dependencies

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,15 @@ export const environment = {
   token_auth_config: {
     apiBase: 'http://fenix:3000'
   },
-  firebaseConfig: {}
+  firebaseConfig: {
+    apiKey: 'AIzaSyAp0YHeLkKZXanR15pUFG5IiCqxpoAWOSo',
+    authDomain: 'aqis-6fe5b.firebaseapp.com',
+    databaseURL: 'https://aqis-6fe5b.firebaseio.com',
+    projectId: 'aqis-6fe5b',
+    storageBucket: '',
+    messagingSenderId: '867099614282',
+    appId: '1:867099614282:web:75f2ed41d0fd8f09'
+  }
 };
 
 /*
